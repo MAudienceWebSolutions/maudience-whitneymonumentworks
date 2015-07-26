29af323ed465b4c0fc303099b56d7c33b9c7c1f9
Pete Lower <pete.lower@gmail.com>
2015-07-26 09:54:20 -0700

    add archive page

---
+++ wordpress/wp-content/themes/maudience-whitneymonumentworks/archive-gallery.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The template for displaying Archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each specific one. For example, Twenty Twelve already
+ * has tag.php for Tag archives, category.php for Category archives, and
+ * author.php for Author archives.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+get_header(); ?>
+
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+			<header class="archive-header">
+				<h1 class="archive-title">Gallery</h1>
+
+				<div class="categories-output">
+
+				</div>
+			</header><!-- .archive-header -->
+
+			<div class="gallery-information-output">
+				<ul class='custom-post-type-list'>
+					<?php
+						$args=array(
+						'post_type' => 'gallery',
+						'post_status' => 'publish',
+						'posts_per_page' => $posts_per_page,
+						'caller_get_posts'=> 1
+						);
+						$my_query = null;
+						$my_query = new WP_Query($args);
+						if( $my_query->have_posts() ) :
+							while ($my_query->have_posts()) : $my_query->the_post(); ?>
+								<li class="custom-post-type-list-item <?php echo get_post_type() ?>">
+									<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+										<?php if ( has_post_thumbnail() ) : ?>
+											<div class="entry-page-image">
+												<?php the_post_thumbnail(); ?>
+											</div><!-- .entry-page-image -->
+										<?php endif; ?>
+										<div class="post-content-wrap">
+										</div>
+									</a>
+								</li>
+								<?php
+							endwhile;
+						endif;
+						wp_reset_query();  // Restore global post data stomped by the_post().
+					?>
+				</ul>
+			</div>
+		<?php endif; ?>
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
+
