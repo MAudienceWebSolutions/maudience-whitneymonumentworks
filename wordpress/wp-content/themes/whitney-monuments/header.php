127efc55e11fc8bb0df903bf2ff6424c80737f81
Pete Lower <pete.lower@gmail.com>
2015-10-20 16:59:30 -0700

    add viewport to header

--- wordpress/wp-content/themes/whitney-monuments/header.php
+++ wordpress/wp-content/themes/whitney-monuments/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	<meta name="viewport" content="width=960">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
